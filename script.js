befda10a1c65191023205d34fee90884620797d8
harshubari <121845828+harshubari@users.noreply.github.com>
2024-03-07 19:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	const trimmedText = text.trim();
+  
+  // Check if the trimmed text is empty
+  if (trimmedText === '') {
+    return '';
+  }
+  
+  // Return the first character
+  return trimmedText[0];
 }
 
 // Do not change the code below
